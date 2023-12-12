b6eb9340c5391012463e7f3dc8956744717f5d6f
Xu Gao <398058068@qq.com>
2023-12-12 11:50:31 +0800

    Release version 0.1.6

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,12 @@ export default defineConfig({
             name: 'docusaurusNotionMdxPlugin',
             // the proper extensions will be added
             fileName: 'docusaurus-notion-mdx-plugin'
-        }
+        },
         // rollupOptions: {
-        //   // 确保外部化处理那些你不想打包进库的依赖
-        //   external: ['vue'],
+          // 确保外部化处理那些你不想打包进库的依赖
+          // external: [
+          //     resolve(__dirname, 'public/readme/**')
+          // ],
         //   output: {
         //    // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         //    globals: {
